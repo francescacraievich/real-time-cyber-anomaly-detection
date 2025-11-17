b7e7f38b120c5866d1144a7644a3b5ad47af7c1b
Mixer97 <169163595+Mixer97@users.noreply.github.com>
2025-11-17 12:54:42 +0100

    add function to united different dataframes

--- feature_engineering/df_formatting/handler_df_formatter.py
+++ feature_engineering/df_formatting/handler_df_formatter.py
@@ -1,15 +1,17 @@
 
+from df_initializing.handler_init_dfs import DataFrameInitializer
 import pandas as pd
 import sys
 import os
+from deprecated import deprecated
 from format_normal_traffic import DataFrameFormatterNormalTraffic
 from format_cowrie_df import DataFrameFormatterCowrie
 from format_dionea_df import DataFrameFormatterDionea
 from format_suricata_df import DataFrameFormatterSuricata
 
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+sys.path.insert(0, os.path.abspath(
+    os.path.join(os.path.dirname(__file__), '..')))
 
-from df_initializing.handler_init_dfs import DataFrameInitializer
 
 class DataFrameFormatter():
 
@@ -23,7 +25,7 @@ class DataFrameFormatter():
             'destination_ip',
             'source_port',
             'destination_port',
-            'timestamp_start', 
+            'timestamp_start',
             'transport_protocol',
             'application_protocol',
             'label',
@@ -31,10 +33,10 @@ class DataFrameFormatter():
         ]
         self.format_all_dfs()
 
-
-
+    @deprecated(reason="Automatically filtering all the columns needed")
     def format_all_NaN_columns(self, threshold: float = 0.8):
-        dataframes = [self.cowrie_df, self.dionea_df, self.suricata_df, self.normal_traffic_df]
+        dataframes = [self.cowrie_df, self.dionea_df,
+                      self.suricata_df, self.normal_traffic_df]
         filtered = []
         for df in dataframes:
             if df is None:
@@ -45,17 +47,17 @@ class DataFrameFormatter():
             filtered.append(df.loc[:, keep_mask])
         return filtered
 
-    
     def format_all_dfs(self):
-        self.cowrie_df = DataFrameFormatterCowrie(self.cowrie_df, self.base_features).format_cowrie_df()
-        self.dionea_df = DataFrameFormatterDionea(self.dionea_df, self.base_features).format_dionea_df()
-        self.suricata_df = DataFrameFormatterSuricata(self.suricata_df, self.base_features).format_suricata_df()
-        self.normal_traffic_df = DataFrameFormatterNormalTraffic(self.normal_traffic_df, self.base_features).format_normal_traffic_df()
-        # self.cowrie_df, self.dionea_df, self.suricata_df, self.normal_traffic_df = self.format_all_NaN_columns()
-        
+        self.cowrie_df = DataFrameFormatterCowrie(
+            self.cowrie_df, self.base_features).format_cowrie_df()
+        self.dionea_df = DataFrameFormatterDionea(
+            self.dionea_df, self.base_features).format_dionea_df()
+        self.suricata_df = DataFrameFormatterSuricata(
+            self.suricata_df, self.base_features).format_suricata_df()
+        self.normal_traffic_df = DataFrameFormatterNormalTraffic(
+            self.normal_traffic_df, self.base_features).format_normal_traffic_df()
 
-
-    def unite_all_dfs(self):
+    def unite_honeypot_and_normal_traffic_dfs(self):
         # concatenate rows from all sources and keep only base_features (as requested)
         combined_df = pd.concat([
             self.cowrie_df[self.base_features],
@@ -63,8 +65,21 @@ class DataFrameFormatter():
             self.suricata_df[self.base_features],
             self.normal_traffic_df[self.base_features]
         ], ignore_index=True, sort=False)
-        combined_df = combined_df.loc[:, self.base_features]  # enforce column order
+        # enforce column order
+        combined_df = combined_df.loc[:, self.base_features]
         return combined_df
+
+    def unite_all_honeypot_dfs(self):
+        # concatenate rows from all honeypot sources and keep only base_features (as requested)
+        combined_df = pd.concat([
+            self.cowrie_df[self.base_features],
+            self.dionea_df[self.base_features],
+            self.suricata_df[self.base_features]
+        ], ignore_index=True, sort=False)
+        # enforce column order
+        combined_df = combined_df.loc[:, self.base_features]
+        return combined_df
+
 
 if __name__ == "__main__":
 
@@ -75,14 +90,20 @@ if __name__ == "__main__":
         normal_traffic_json_path="data/normal_traffic/benign_traffic_fixed.json"
     )
 
-    df_cowrie, df_dionea, df_suricata, df_normal_traffic = df_initializer.initialize_dfs(sample_size=1000)
+    df_cowrie, df_dionea, df_suricata, df_normal_traffic = df_initializer.initialize_dfs(
+        sample_size=1000)
 
-    df_formatter = DataFrameFormatter(df_cowrie, df_dionea, df_suricata, df_normal_traffic)
+    df_formatter = DataFrameFormatter(
+        df_cowrie, df_dionea, df_suricata, df_normal_traffic)
 
-    print("Formatted Cowrie DataFrame:" , df_formatter.cowrie_df['label'].head())
-    print("Formatted Dionea DataFrame:" , df_formatter.dionea_df['label'].head())
-    print("Formatted Suricata DataFrame:" , df_formatter.suricata_df['label'].head())
-    print("Formatted Normal Traffic DataFrame:" , df_formatter.normal_traffic_df['label'].head())
+    print("Formatted Cowrie DataFrame:",
+          df_formatter.cowrie_df['label'].head())
+    print("Formatted Dionea DataFrame:",
+          df_formatter.dionea_df['label'].head())
+    print("Formatted Suricata DataFrame:",
+          df_formatter.suricata_df['label'].head())
+    print("Formatted Normal Traffic DataFrame:",
+          df_formatter.normal_traffic_df['label'].head())
 
     print(df_formatter.cowrie_df.shape[1])
     print(df_formatter.dionea_df.shape[1])
@@ -95,7 +116,7 @@ if __name__ == "__main__":
     print("Normal Traffic Columns:", df_formatter.normal_traffic_df.columns)
 
     combined_df = df_formatter.unite_all_dfs()
-    print("Combined DataFrame:" , combined_df.head())
+    print("Combined DataFrame:", combined_df.head())
     print("Combined DataFrame shape:", combined_df.shape)
     print("Combined DataFrame columns:", combined_df.columns)
 
